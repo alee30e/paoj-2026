3725604aceb91f5678e5a71e29485c886cd8adf2
alee30e <ale_2006e@yahoo.com>
2026-03-23 10:12:37 +0200

    biblioteca

--- src/com/pao/laboratory05/biblioteca/Carte.java
+++ src/com/pao/laboratory05/biblioteca/Carte.java
@@ -1,4 +1,35 @@
 package com.pao.laboratory05.biblioteca;
 
-public class Carte {
+public class Carte implements Comparable<Carte>{
+    private String titlu;
+    private String autor;
+    private int an;
+    private double rating;
+
+    public Carte(String titlu, String autor, int an, double rating){
+        this.titlu = titlu;
+        this.autor = autor;
+        this.an = an;
+        this.rating = rating;
+    }
+    public String getTitlu(){
+        return this.titlu;
+    }
+    public String getAutor(){
+        return this.autor;
+    }
+    public int getAn(){
+        return this.an;
+    }
+    public double getRating(){
+        return this.rating;
+    }
+    @Override
+    public String toString(){
+        return "Carte{titlu='" + titlu + "', autor='" + autor +  "', an=" + an + ", rating=" + rating +"}";
+    }
+    @Override
+    public int compareTo(Carte other){
+        return Double.compare(this.getRating(), other.getRating());
+    }
 }
