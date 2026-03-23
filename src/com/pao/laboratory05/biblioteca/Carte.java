85b0f3e2d22b096ae7fe0b4ec8ebf937d930e783
alee30e <ale_2006e@yahoo.com>
2026-03-23 09:26:57 +0200

    playlist

---
+++ src/com/pao/laboratory05/biblioteca/Carte.java
@@ -0,0 +1,4 @@
+package com.pao.laboratory05.biblioteca;
+
+public class Carte {
+}
---
+++ src/com/pao/laboratory05/playlist/Playlist.java
@@ -0,0 +1,43 @@
+package com.pao.laboratory05.playlist;
+
+
+import java.util.Arrays;
+
+public class Playlist {
+    private String name;
+    private Song[] songs = new Song[0];
+    public String getName(){
+        return name;
+    }
+    public Playlist(String name){
+        this.name = name;
+    }
+    public void addSong(Song song){
+        Song[] temp = new Song[songs.length + 1];
+        System.arraycopy(songs, 0, temp, 0, songs.length);
+        temp[temp.length - 1] = song;
+        songs = temp;
+    }
+    public void printSortedByTitle(){
+        Song[] temp = songs;
+        Arrays.sort(temp);
+        for (Song s : temp){
+            System.out.println(s);
+        }
+    }
+
+    public void printSortedByDuration(){
+        Song[] temp = songs;
+        Arrays.sort(temp, new SongDurationComparator());
+        for (Song s : temp) {
+            System.out.println(s);
+        }
+    }
+    int getTotalDuration(){
+        int total = 0;
+        for (Song s : songs){
+            total += s.durationSeconds();
+        }
+        return total;
+    }
+}
