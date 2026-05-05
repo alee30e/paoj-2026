96ffba8aa8715c260bd70b93705f26ef1a1ba1e6
alee30e <ale_2006e@yahoo.com>
2026-05-05 21:32:20 +0300

    ex1

--- src/com/pao/laboratory09/exercise1/Main.java
+++ src/com/pao/laboratory09/exercise1/Main.java
@@ -9,7 +9,68 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Implementează conform Readme.md
         //
+        Scanner sc = new Scanner(System.in);
+
+        int N = Integer.parseInt(sc.nextLine());
+
+        List<Tranzactie> lista = new ArrayList<>();
+
+        for (int i =0; i < N; i++){
+            String linie = sc.nextLine();
+
+            String[] cuv = linie.split(" ");
+            int id = Integer.parseInt(cuv[0]);
+            String data = cuv[2];
+            double suma = Double.parseDouble(cuv[1]);
+            String contSursa = cuv[3];
+            String contDestinatie = cuv[4];
+            TipTranzactie tip = TipTranzactie.valueOf(cuv[5]);
+
+            Tranzactie t = new Tranzactie(id, suma, data, contSursa, contDestinatie, tip);
+
+            lista.add(t);
+        }
+
+        while (sc.hasNextLine()){
+            String linie = sc.nextLine();
+            if (linie.isEmpty()) continue;
+
+            if (linie.equals("LIST")){
+                for (Tranzactie t : lista) System.out.println(t);
+            }
+            else if (linie.startsWith("FILTER")){
+                String[] cuv = linie.split(" ");
+                String data = cuv[1];
+
+                boolean gasit = false;
+
+                for (Tranzactie t : lista) {
+                    if (t.getData().startsWith(data)){
+                        System.out.println(t);
+                        gasit = true;
+                    }
+                }
+                if (!gasit) System.out.println("Niciun rezultat.");
+            }
+            else if (linie.startsWith("NOTE")){
+                String[] cuv = linie.split(" ");
+                int id = Integer.parseInt(cuv[1]);
+
+                boolean gasit = false;
+
+                for (Tranzactie t : lista){
+                    if (t.getId() == id){
+                        System.out.println("NOTE[" + id + "]: "+ t.getNote());
+                        gasit = true;
+                        break;
+                    }
+                }
+                if (!gasit) System.out.println("NOTE["+id+"]: not found");
+            }
+        }
         // 1. Citește N din stdin, apoi cele N tranzacții (id suma data contSursa contDestinatie tip)
+
+
         // 2. Setează câmpul note = "procesat" pe fiecare tranzacție înainte de serializare
         // 3. Serializează lista de tranzacții în OUTPUT_FILE cu ObjectOutputStream (try-with-resources)
         // 4. Deserializează lista din OUTPUT_FILE cu ObjectInputStream (try-with-resources)
@@ -24,6 +85,8 @@ public class Main {
         //   [id] data tip: suma RON | contSursa -> contDestinatie
         //   Ex: [1] 2024-01-15 CREDIT: 1500.00 RON | RO01SRC1 -> RO01DST1
 
-        System.out.println("TODO: implementează exercițiul 1");
+
+
+//        System.out.println("TODO: implementează exercițiul 1");
     }
 }
---
+++ src/com/pao/laboratory09/exercise1/Tranzactie.java
@@ -0,0 +1,59 @@
+package com.pao.laboratory09.exercise1;
+
+import java.io.Serializable;
+
+public class Tranzactie implements Serializable {
+    private int id;
+    private double suma;
+    private String data;
+    private String contSursa;
+    private String contDestinatie;
+    private TipTranzactie tip;
+    transient String note;
+    private static long serialVersionUID = 1L;
+
+    public Tranzactie(int id, double suma, String data, String contSursa, String contDestinatie, TipTranzactie tip){
+        this.id = id;
+        this.suma = suma;
+        this.data = data;
+        this.contSursa = contSursa;
+        this.contDestinatie = contDestinatie;
+        this.tip = tip;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public double getSuma() {
+        return suma;
+    }
+
+    public String getContSursa() {
+        return contSursa;
+    }
+
+    public String getContDestinatie() {
+        return contDestinatie;
+    }
+
+    public TipTranzactie getTip() {
+        return tip;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s: %.2f RON | %s -> %s", id, data, tip, suma, contSursa, contDestinatie);
+    }
+}
